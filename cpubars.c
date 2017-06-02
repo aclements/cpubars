8231966ecb3e124732e6c402ec7666dd8d832006
Tim Creech <tcreech@umd.edu>
2017-06-02 17:15:54 -0400

    fix return type of cpustats_subtract1

--- cpubars.c
+++ cpubars.c
@@ -275,7 +275,7 @@ cpustats_read(struct cpustats *out)
                 epanic("failed to seek /proc/stat");
 }
 
-static bool
+static void
 cpustats_subtract1(struct cpustat *out,
                    const struct cpustat *a, const struct cpustat *b)
 {
